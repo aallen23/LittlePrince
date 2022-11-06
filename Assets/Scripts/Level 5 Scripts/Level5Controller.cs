60e0e7a719ff8d45adfcb2e09afc38a05b155e1f
aallen23 <allenanna917@gmail.com>
2022-11-05 22:10:39 -0400

    start level5 coding

--- Assets/Scripts/Level 5 Scripts/Level5Controller.cs
+++ Assets/Scripts/Level 5 Scripts/Level5Controller.cs
@@ -5,6 +5,7 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+
 public class Level5Controller : MonoBehaviour
 {
     [SerializeField] Touch touch;
@@ -17,6 +18,8 @@ public class Level5Controller : MonoBehaviour
     public float lerp;
     public bool moving;
 
+    public bool carrying;
+
     public TextMeshProUGUI gameOverText;
     public Button retryButton;
     public GameObject blackUIPanel;
@@ -25,7 +28,7 @@ public class Level5Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        lerp = 1.5f;
     }
 
     // Update is called once per frame
@@ -39,7 +42,8 @@ public class Level5Controller : MonoBehaviour
                 if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
                 {
                     Vector2 touchPos = Camera.main.ScreenToWorldPoint(new Vector2(touch.position.x, touch.position.y));
-                    transform.position = Vector2.Lerp(transform.position, touchPos, Time.deltaTime);
+                    StartCoroutine(Lerp(gameObject.transform.position, touchPos));
+                    //transform.position = Vector2.Lerp(transform.position, touchPos, Time.deltaTime);
                     Debug.Log("Told to move");
                 }
             }
@@ -49,6 +53,70 @@ public class Level5Controller : MonoBehaviour
         {
             Ending();
         }
+        //if (Input.touchCount > 0)
+        //{
+        //    touch = Input.GetTouch(0);
+        //    if (touch.phase == TouchPhase.Began)
+        //    {
+        //        Debug.Log("touched");
+        //        Ray ray = Camera.main.ScreenPointToRay(touch.position);
+        //        RaycastHit hit;
+        //        if (Physics.Raycast(ray.origin, ray.direction, out hit))
+        //        {
+        //            if (hit.collider.gameObject != null)
+        //            {
+        //                Debug.Log("collided");
+        //                if (hit.collider.gameObject.CompareTag("Watering Can"))
+        //                {
+        //                    Debug.Log("watering can");
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("collided");
+        if (other.gameObject.CompareTag("Watering Can"))
+        {
+            PickUpWateringCan(other.gameObject);
+        }
+        if (other.CompareTag("Spikes"))
+        {
+
+        }
+        if (other.CompareTag("Rose") && carrying)
+        {
+            //water rose here
+        }
+    }
+
+    public void PickUpWateringCan(GameObject wateringCan)
+    {
+        wateringCan.transform.parent = gameObject.transform;
+        wateringCan.transform.localPosition = new Vector2(-1, 0);
+        carrying = true;
+        Debug.Log("picked up watering can");
+    }
+
+    public void ResetWateringCan()
+    {
+        //reset watering can to original location once rose is watered
+    }
+
+    IEnumerator Lerp(Vector3 startPos, Vector3 targetPos)
+    {
+        float time = 0;
+        while (time < lerp)
+        {
+            transform.position = Vector3.Lerp(startPos, targetPos, time / lerp);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPos;
     }
 
     public void GameOver()
