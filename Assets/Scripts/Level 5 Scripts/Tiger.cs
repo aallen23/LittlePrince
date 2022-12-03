c292f0a3eacf87ca8b2f20657ac961b4e12cb2d1
aallen23 <allenanna917@gmail.com>
2022-12-03 14:28:43 -0500

    tiger updates

--- Assets/Scripts/Level 5 Scripts/Tiger.cs
+++ Assets/Scripts/Level 5 Scripts/Tiger.cs
@@ -1,24 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
 public class Tiger : MonoBehaviour
 {
-
+    public Vector2 targetPos;
+    private Vector2 startPos;
+    private float speed;
+    private bool moving;
+    private int returning;
     private Spikes spikes;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
+        speed = 2.0f;
+        moving = true;
+        returning = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        while (moving)
+        {
+            transform.Translate(Vector3.left * targetPos.x * returning * speed * Time.deltaTime);
+            transform.Translate(Vector3.up * targetPos.y * returning * speed * Time.deltaTime);
+        }
     }
-
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -27,8 +39,16 @@ public class Tiger : MonoBehaviour
             spikes = other.gameObject.GetComponent<Spikes>();
             if(spikes.triggered == true)
             {
-
+                moving = false;
+                returning = -1;
             }
+        }
+
+        if (other.CompareTag("Flower"))
+        {
+            //play sound effect & animation
+            //decrease health
+            returning = -1;
         }
     }
 
